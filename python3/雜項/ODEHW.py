43394acab658e439be342b9452be7b6fc3de87bb
leon890820 <52538091+leon890820@users.noreply.github.com>
2020-05-11 20:50:00 +0800

    ODE

--- python3/雜項/ODEHW.py
+++ python3/雜項/ODEHW.py
@@ -1,16 +1,21 @@
 import math 
-x=1
+def fun(x,y):
+    return 2*y
+
+x=0
 y=1
 h=0.05
-
-xs=[]
-ys=[]
-c=1
-while(x<1.5):
-    y1=y+h*(x*y**2-y/x)
-    x1=x+h
-    y=y+(h*((x*y**2-y/x)+(x1*y1**2-y1/x1)))/2
+print("x=",x+h,"y=",y)
+while(x<0.15):
+    k1=fun(x,y)
+    k2=fun(x+0.5*h,y+0.5*h*k1)
+    k3=fun(x+0.5*h,y+0.5*h*k2)
+    k4=fun(x+h,y+h*k3)
+    y=y+(h/6)*(k1+2*k2+2*k3+k4)
+    if x==0:
+        print(k1,k2,k3,k4)
+    print("x=",x+h,"y=",y)
     x+=h
-    print(c,end=" : ")
-    print("{:>2}".format(x),y)
-    c+=1
+
+    
+
